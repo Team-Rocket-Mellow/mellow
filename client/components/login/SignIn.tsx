b9e0f02da24e0ece4022b642de5afeaf9bb985f5
John Chon <johnchon94@gmail.com>
2022-05-13 17:03:52 -0700

    css sign-in page

--- client/components/login/SignIn.tsx
+++ client/components/login/SignIn.tsx
@@ -13,17 +13,19 @@ function SignIn() {
   return ( 
     <>
       <div id='intro_1'>
-        <h3>Ready to get started?</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <button onClick={handleSubmit(onRegister)}>Start Trial</button>
+        <div>
+          <h3>Ready to get started?</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <button className="start-trial-button" onClick={handleSubmit(onRegister)}>Start Trial</button>
+        </div>
       </div>
 
       <div id='intro_2'>
-        <h4>Already a user?</h4>
         <form>
-          <input {...register('email')} placeholder='email' />
-          <input {...register('password')} placeholder='password' />
-          <button onClick={handleSubmit(onSubmit)}>Sign In</button>
+          <h4>Already a user?</h4>
+          <input className="email-input" {...register('email')} placeholder='email' />
+          <input className="password-input" {...register('password')} placeholder='password' />
+          <button className="sign-in-button" onClick={handleSubmit(onSubmit)}>Sign In</button>
         </form>
       </div>
     </>
