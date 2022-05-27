c4e5cdf6de4b7a1d3fd631bf5cc7f4c8cfcf07a3
meow <meow@>
2022-05-26 18:26:31 -0700

    update

--- server/websocket.ts
+++ server/websocket.ts
@@ -4,20 +4,17 @@ import { nanoid } from "nanoid"
 const server = new WebSocket.Server({ port: 2049 })
 const clients = new Map()
 
-server.on(
-   "connection",
-   (ws: WebSocket) => {
-      const id = nanoid(36)
-      clients.set(id, ws)
-      ws.on("message", (message: string) => {
-         const data = JSON.parse(message)
-         console.log(data)
-         clients.forEach((client) => {
-            client.send(message)
-         })
+server.on("connection", (ws: WebSocket) => {
+   const id = nanoid(36)
+   clients.set(id, ws)
+   ws.on("message", (message: string) => {
+      const data = JSON.parse(message)
+      console.log(data)
+      clients.forEach((client) => {
+         client.send(message)
       })
-      ws.on("close", () => {
-         clients.delete(id)
-      })
-   }
-)
+   })
+   ws.on("close", () => {
+      clients.delete(id)
+   })
+})
