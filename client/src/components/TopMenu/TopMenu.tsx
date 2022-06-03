c6250c0ae29e6be8925b5258b860faa80d8d6fc9
meow <meow@>
2022-06-03 10:22:02 -0700

    Update: [TopMenu] & Component: [Icon]

--- client/src/components/TopMenu/TopMenu.tsx
+++ client/src/components/TopMenu/TopMenu.tsx
@@ -2,6 +2,7 @@ import "./TopMenu.css"
 import { useState } from "react"
 import { useSetRecoilState, useRecoilValue } from "recoil"
 import { modal_is_active, todos_view, home } from "../../state/atoms"
+import Icon from "../assets/Icon"
 
 // —————————————————————————————————————————————————————————————————————————————
 // Component
@@ -17,18 +18,22 @@ function TopMenu() {
 
   return (
     <header id='NavBar'>
-      <i className='material-symbols-rounded'>menu</i>
-      <i className='material-symbols-rounded' onClick={goInbox}>home</i>
+      <nav>
+        <Icon>menu</Icon>
+        <Icon onClick={goInbox}>home</Icon>
+      </nav>
       <input
         type='search'
-        tabIndex={-1}
         placeholder='/  to search'
+        tabIndex={-1}
         value={text}
         onChange={e => setText(e.target.value)}
       />
-      <i className='material-symbols-rounded' onClick={openModal}>add</i>
-      <i className='material-symbols-rounded'>settings</i>
-      <i className='material-symbols-rounded'>account_circle</i>
+      <nav>
+        <Icon onClick={openModal}>add</Icon>
+        <Icon>settings</Icon>
+        <Icon>account_circle</Icon>
+      </nav>
     </header>
   )
 }
---
+++ client/src/components/assets/Icon.tsx
@@ -0,0 +1,5 @@
+function Icon({ children, ...props }) {
+   return <i className='material-symbols-rounded' {...props}>{children}</i>
+}
+
+export default Icon
