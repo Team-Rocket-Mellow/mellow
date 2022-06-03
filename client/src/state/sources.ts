4012d0b354ee76e371742663480e106bd1d2851b
meow <meow@>
2022-06-03 13:56:58 -0700

    Added time stuff

---
+++ client/src/state/sources.ts
@@ -0,0 +1,32 @@
+import { current_date } from "./atoms"
+
+class Clock {
+   day   : number
+   month : number
+   year  : number
+
+   constructor() {
+      const now = new Date()
+      this.day   = now.getDate()
+      this.month = now.getMonth()
+      this.year  = now.getFullYear()
+   }
+
+   tick(offset=60_000) {
+      const t0 = new Date()
+      const ms_to_midnight = t0.getTime() - t0.setHours(0,0,0,0)
+      const id = setTimeout(() => {
+         this.tick(0)
+      }, ms_to_midnight - offset)
+   }
+
+   parseDate(date:Date) {
+      return {
+         day   : date.getDate(),
+         month : date.getMonth(),
+         year  : date.getFullYear()
+      }
+   }
+}
+
+export default Clock
