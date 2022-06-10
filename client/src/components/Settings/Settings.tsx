f6ff37accf3ef9827df7db983ff634f7c59de86e
meow <meow@>
2022-06-10 02:44:58 -0700

    update

---
+++ client/src/components/Settings/Settings.tsx
@@ -0,0 +1,43 @@
+import { TodoView, Theme, } from "../../state/types"
+
+// —————————————————————————————————————————————————————————————————————————————
+// Environment
+
+type Settings = {
+   theme: Theme
+   home: TodoView
+}
+
+const themes:Theme[] = ["light", "dark", "contrast"]
+const views:TodoView[] = ["all", "inbox", "today", "upcoming", "done", "trash"]
+
+// —————————————————————————————————————————————————————————————————————————————
+// Constituent
+
+function Select({ label, options }: { label: string, options: string[] }) {
+   return (
+      <label htmlFor={label}>
+         {label}
+         <select name={label}>
+            { options.map(o => <option value={o} key={o}>{o}</option>) }
+         </select>
+      </label>
+   )
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Settings
+
+function Settings() {
+   return (
+      <form id='Settings'>
+         <Select label='Theme' options={themes} />
+         <Select label='Home' options={views} />
+      </form>
+   )
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Export
+
+export default Settings
