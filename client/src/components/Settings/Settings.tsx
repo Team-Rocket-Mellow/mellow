49e7595aef931925cf6ff64a432b8b89d99af51d
meow <meow@>
2022-06-11 09:18:30 -0700

    update

--- client/src/components/Settings/Settings.tsx
+++ client/src/components/Settings/Settings.tsx
@@ -4,8 +4,8 @@ import { TodoView, Theme, } from "../../state/types"
 // Environment
 
 type Settings = {
-   theme: Theme
-   home: TodoView
+  theme: Theme
+  home: TodoView
 }
 
 const themes:Theme[] = ["light", "dark", "contrast"]
@@ -15,26 +15,26 @@ const views:TodoView[] = ["all", "inbox", "today", "upcoming", "done", "trash"]
 // Constituent
 
 function Select({ label, options }: { label:string, options:string[] }) {
-   return (
-      <label htmlFor={label}>
-         {label}
-         <select name={label}>
-            { options.map(o => <option value={o} key={o}>{o}</option>) }
-         </select>
-      </label>
-   )
+  return (
+    <label htmlFor={label}>
+      {label}
+      <select name={label}>
+        { options.map(o => <option value={o} key={o}>{o}</option>) }
+      </select>
+    </label>
+  )
 }
 
 // —————————————————————————————————————————————————————————————————————————————
 // Settings
 
 function Settings() {
-   return (
-      <form id='Settings'>
-         <Select label='Theme' options={themes} />
-         <Select label='Home' options={views} />
-      </form>
-   )
+  return (
+    <form id='Settings'>
+      <Select label='Theme' options={themes} />
+      <Select label='Home' options={views} />
+    </form>
+  )
 }
 
 // —————————————————————————————————————————————————————————————————————————————
