3d3afa7ee5520c984ace91cff6fb544144f7585f
meow <meow@>
2022-06-14 00:06:18 -0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css"
 import { RecoilRoot, } from "recoil"
+import { BrowserRouter } from 'react-router-dom'
 import Home from "./pages/Home"
 
 // —————————————————————————————————————————————————————————————————————————————
 // Component
 
 function App() {
-  return <>
-    <RecoilRoot>
-      <Home />
-    </RecoilRoot>
-  </>
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Home />
+      </RecoilRoot>
+    </BrowserRouter>
+  )
 }
 
 // —————————————————————————————————————————————————————————————————————————————
