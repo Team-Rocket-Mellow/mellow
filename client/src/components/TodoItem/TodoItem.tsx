d70b8ef28158fdfdf737f78f92745665ad9c7692
meow <meow@>
2022-06-21 12:58:25 -0700

    update

--- client/src/components/TodoItem/TodoItem.tsx
+++ client/src/components/TodoItem/TodoItem.tsx
@@ -12,13 +12,14 @@ function TodoItem({ id, text, done, due, overdue }: TodoElement) {
   const [todos, setTodos] = useRecoilState(todos_list)
   const now = useRecoilValue(current_date)
   const flipDone = () => setTodos(todos.map(t => t.id === id ? { ...t, done: !t.done } : t))
+  const isDone = done ? "done" : ""
 
   return (
     <div className="TodoItem">
       <span className="left" onClick={flipDone}>
           <Icon>check_box_outline_blank</Icon>
-          <Icon className={`checkbox ${done && "done"}`}>check_box</Icon>
-        <span className="text">{text}</span>
+          <Icon className={`checkbox ${isDone}`}>check_box</Icon>
+        <span className={`text ${isDone}`}>{text}</span>
       </span>
       <span className={overdue ? "overdue" : ""}>
         { dayMonthYearString(due) }
