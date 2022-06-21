b8e6152099497f84400ba5ce03844d73684fb71b
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-06-20 21:35:51 -0700

    CSS refactor (#52)

--- client/src/components/TodoItem/TodoItem.tsx
+++ client/src/components/TodoItem/TodoItem.tsx
@@ -1,5 +1,4 @@
 import "./TodoItem.css"
-import { useState } from "react"
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { todos_list, current_date } from "../../state/atoms"
 import { TodoElement } from "../../state/types"
@@ -11,18 +10,14 @@ import Icon from "../assets/Icon"
 
 function TodoItem({ id, text, done, due, overdue }: TodoElement) {
   const [todos, setTodos] = useRecoilState(todos_list)
-  const [isHover, setHover] = useState(false)
   const now = useRecoilValue(current_date)
-  const itemClass = done ? "done left" : "active left"
   const flipDone = () => setTodos(todos.map(t => t.id === id ? { ...t, done: !t.done } : t))
-  const enter = () => setHover(true)
-  const exit = () => setHover(false)
 
   return (
     <div className="TodoItem">
-      <span className={itemClass} onClick={flipDone}>
+      <span className="left" onClick={flipDone}>
           <Icon>check_box_outline_blank</Icon>
-          <Icon className={`checkbox ${done ? "done" : ""}`}>check_box</Icon>
+          <Icon className={`checkbox ${done && "done"}`}>check_box</Icon>
         <span className="text">{text}</span>
       </span>
       <span className={overdue ? "overdue" : ""}>
