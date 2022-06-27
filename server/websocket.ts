ca0d615ace6b26f01a7dd639d3e27d051e33c52c
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-06-26 18:02:27 -0700

    Server WIP (#58)

--- server/websocket.ts
+++ server/websocket.ts
@@ -1,20 +1,63 @@
 import WebSocket from "ws"
+import SyncSet from "../crdt/set"
 import { nanoid } from "nanoid"
 
-const server = new WebSocket.Server({ port: 2049 })
-const clients = new Map()
+// —————————————————————————————————————————————————————————————————————————————
+// Types
 
-server.on("connection", (ws: WebSocket) => {
+type Peers = { [username:string]: string }
+type Clock = { [id:string]: number }
+
+// -----------------------------------------------------------------------------
+// Actions
+
+type Actions = Remove | Initial
+
+type Remove = {
+   type  : "REMOVE"
+   clock : Clock
+   id    : string
+}
+
+type Add = {
+   type  : "ADD"
+   clock : Clock
+   id    : string
+   value : string
+}
+
+type Initial = {
+   type  : "INITIAL"
+   clock : Clock
+   id    : string
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Persistence
+
+const clients = new Map()
+const set = new SyncSet()
+
+// —————————————————————————————————————————————————————————————————————————————
+// Websocket Server
+
+const server = new WebSocket.Server({ port: 8080 })
+
+server.on("connection", onConnect)
+
+function onConnect(ws:WebSocket) {
    const id = nanoid(36)
    clients.set(id, ws)
-   ws.on("message", (message: string) => {
+
+   ws.on("message", (message:string) => {
       const data = JSON.parse(message)
       console.log(data)
-      clients.forEach((client) => {
+      clients.forEach(client => {
          client.send(message)
       })
    })
+
    ws.on("close", () => {
       clients.delete(id)
    })
-})
+}
