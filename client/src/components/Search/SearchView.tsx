c67d10d48556b790b93fd9631486da72ce9d09e6
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-07-08 10:52:16 -0700

    + Search (#62)
    Added Search component and fixed React Router issues.

--- client/src/components/Search/SearchView.tsx
+++ client/src/components/Search/SearchView.tsx
@@ -1,20 +1,29 @@
 import { useRecoilValue } from "recoil"
 import { todos as todos_data } from "../../state/selectors"
-import { search } from "../../state/atoms"
+import { search, left_menu } from "../../state/atoms"
 import TodoItem from "../TodoItem/TodoItem"
+import "./SearchView.css"
 
 function SearchView() {
-   const query = useRecoilValue(search)
+   const isMenuOn = useRecoilValue(left_menu);
+   const query = useRecoilValue(search);
    const todos = useRecoilValue(todos_data)
-      .filter(todo => todo.text.includes(query))
+      .filter(todo => todo.text.toLowerCase().includes(query.toLowerCase()));
+
+   let queryFound = todos.length > 0;
 
    return (
-      <main>
-         <h1>Search: {query}</h1>
-         {
-            todos.map((t, i) => <TodoItem key={i} {...t} />)
-         }
+      <>
+      {queryFound ?
+         <main id="results-view" className={isMenuOn ? 'open' : 'close'}>
+            <h2>{`Results for "${query}"`}</h2>
+            {todos.map((t, i) => <TodoItem key={i} {...t} />)}
+         </main> :
+         <main id="results-view" className={isMenuOn ? 'open' : 'close'}>
+            <h2>{`No matches found for "${query}"`}</h2>
       </main>
+      }
+   </>
    )
 }
 
