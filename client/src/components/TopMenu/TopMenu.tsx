f6b421f82d9b5c4950dc758deae816961f75391e
meow <meow@>
2022-07-10 19:35:04 -0700

    update

--- client/src/components/TopMenu/TopMenu.tsx
+++ client/src/components/TopMenu/TopMenu.tsx
@@ -2,7 +2,7 @@ import "./TopMenu.css"
 import { Link } from "react-router-dom"
 
 import { useSetRecoilState, useRecoilValue } from "recoil"
-import { add_is_active, todos_view, home, left_menu } from "../../state/atoms"
+import { add_is_active, todos_view, home, left_menu, setting_is_active } from "../../state/atoms"
 
 import Icon from "../assets/Icon"
 import SearchInput from "../Search/SearchInput"
@@ -13,12 +13,14 @@ import Tooltip from "../assets/Tooltip"
 
 function TopMenu() {
   const toggleMenu = useSetRecoilState(left_menu)
+  const toggleSettings = useSetRecoilState(setting_is_active)
   const toggleAddModal = useSetRecoilState(add_is_active)
   const setView = useSetRecoilState(todos_view)
   const defaultHome = useRecoilValue(home)
   const Δmenu = () => toggleMenu($ => !$)
   const goHome = () => setView(defaultHome)
   const openModal = () => toggleAddModal(true)
+  const openSettings = () => toggleSettings($ => !$)
 
   return (
     <header id='NavBar'>
@@ -37,7 +39,7 @@ function TopMenu() {
         <Tooltip content="add todo" hotkey="q">
           <Icon onClick={openModal}>add</Icon>
         </Tooltip>
-        <Icon>settings</Icon>
+        <Icon onClick={openSettings}>settings</Icon>
         <Icon>account_circle</Icon>
       </nav>
     </header>
